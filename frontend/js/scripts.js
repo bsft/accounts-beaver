05c8bc923fc530fb200997db59b52a63dddbb8a2
Richard Coucoules <richardcoucoules@gmail.com>
2021-04-08 11:13:00 -0700

    Add boilerplate code for frontend

---
+++ frontend/js/scripts.js
@@ -0,0 +1,4 @@
+// Boilerplate: replace this with production code
+content = document.createElement("div");
+content.textContent = "JS is working!";
+document.querySelector("#content").append(content);
