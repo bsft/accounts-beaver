c99c8aa4a64565fbdd4a838c1ea7755a478239a0
Alex Deatherage <alexdeatherage4829@gmail.com>
2021-04-10 16:56:08 -0700

    Experiment with PDFKit

--- backend/modules/generate-pdf.js
+++ backend/modules/generate-pdf.js
@@ -1,5 +1,14 @@
-function generatePDF() {
-  // TODO: implement generatePDF function
-}
+// function generatePDF() {
+//   // TODO: implement generatePDF function
+//   const fs = 
+// }
 
-module.exports = generatePDF;
+// module.exports = generatePDF;
+
+const fs = require('fs')
+const PDFDocument = require('pdfkit');
+
+let pdfDoc = new PDFDocument;
+pdfDoc.pipe(fs.createWriteStream('SampleDocument.pdf'));
+pdfDoc.text("My Sample PDF Document");
+pdfDoc.end();
