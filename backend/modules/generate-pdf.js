ddf4b6306dd93ee59dd0920bf2d57ed48610aee9
Richard Coucoules <richardcoucoules@gmail.com>
2021-04-11 08:43:21 -0700

    Add invoice generation

--- backend/modules/generate-pdf.js
+++ backend/modules/generate-pdf.js
@@ -1,9 +1,115 @@
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
 
+function generateInvoiceFromRequestObject(body) {
+  const shipping = {
+    name: body["recipient-name"],
+    address: body["recipient-address"],
+    city: body["recipient-city"],
+    state: body["recipient-state"],
+    country: body["recipient-country"],
+    postal_code: body["recipient-zip"],
+    email: body["recipient-email"],
+  };
+  const sender = {
+    name: body["sender-name"],
+    address: body["sender-address"],
+    city: body["sender-city"],
+    state: body["sender-state"],
+    country: body["sender-country"],
+    postal_code: body["sender-zip"],
+    email: body["sender-email"],
+  };
+  const items = [];
+  const seen_items = new Set();
+  for (const key of Object.keys(body)) {
+    if (key.startsWith("item")) {
+      const itemNum = key.slice(0, key.indexOf("-"));
+      if (!seen_items.has(itemNum)) {
+        seen_items.add(itemNum);
+        items.push({
+          item_description: body[`${itemNum}-description`],
+          quantity: body[`${itemNum}-quantity`],
+          amount: body[`${itemNum}-amount`],
+        });
+      }
+    }
+  }
+  const due = new Date(body["due-date"]);
+
+  return generateInvoice(
+    body["invoice-num"],
+    shipping,
+    sender,
+    items,
+    due,
+    body["shipping-costs"],
+    body["notes"],
+    body["amount-paid"],
+    body["terms"],
+    body["tax"]
+  );
+}
+
+function generateInvoice(
+  invoice_nb,
+  shipping,
+  sender,
+  items,
+  due,
+  shipping_costs,
+  notes,
+  amount_paid = 0,
+  terms = "Net 30",
+  tax = 8
+) {
+  // calculate subtotal
+  let subtotal = 0;
+  for (let i = 0; i < items.length; i++) {
+    subtotal = subtotal + items[i].amount * items[i].quantity;
+  }
+
+  return {
+    invoice_nb,
+    due, // Date object
+    terms, // "PIA" || "CIA" || "Upon Receipt" || "Net X" || "EOM" || "X MFI" || "X Percent Upfront"
+    shipping,
+    // shipping === {
+    //   name: "John Doe",
+    //   address: "1234 Main Street",
+    //   city: "San Francisco",
+    //   state: "CA",
+    //   country: "US",
+    //   postal_code: 12345,
+    //   email: "john@doe.com",
+    // }
+    sender,
+    // sender === {
+    //   name: "Accounts Beaver",
+    //   address: "1500 SW Jefferson Way",
+    //   city: "Corvallis",
+    //   state: "OR",
+    //   country: "US",
+    //   postal_code: 97331,
+    // }
+    items,
+    // items === [
+    //   {
+    //     item_description: "Toner Cartridge",
+    //     quantity: 2,
+    //     amount: 6000,
+    //   }, ...
+    // ]
+    subtotal, // in cents
+    shipping_costs, // in cents
+    amount_paid, // in cents
+    tax, // percent
+    notes,
+  };
+}
+
 function generatePDF(invoice, path) {
-  // TODO: implement generatePDF function
-  let doc = new PDFDocument({ size: "A4", margin: 50});
+  let doc = new PDFDocument({ size: "Letter", margin: 50 });
   doc.pipe(fs.createWriteStream(path));
 
   generateHeader(doc, invoice);
@@ -11,31 +117,29 @@ function generatePDF(invoice, path) {
   generateInvoiceTable(doc, invoice);
   generateFooter(doc, invoice);
   doc.end();
-
 }
 
 function generateHeader(doc, invoice) {
-  let send = invoice.sender
+  let send = invoice.sender;
   doc
     .fontSize(20)
     .text(send.name, 110, 57)
     .fontSize(10)
     .text(send.name, 200, 50, { align: "right" })
     .text(send.address, 200, 65, { align: "right" })
-    .text(`${send.city}, ${send.state}, ${send.postal_code}`, 200, 80, { align: "right" })
+    .text(`${send.city}, ${send.state}, ${send.postal_code}`, 200, 80, {
+      align: "right",
+    })
     .moveDown();
 }
 
-
 function generateCustomerInfo(doc, invoice) {
-  doc
-    .fontSize(20)
-    .text("Invoice", 50, 160);
+  doc.fontSize(20).text("Invoice", 50, 160);
 
   generateHomeRow(doc, 185);
 
   const position = 200; //Y-coordinate position
-  let ship = invoice.shipping
+  let ship = invoice.shipping;
 
   doc
     .fontSize(10)
@@ -48,26 +152,21 @@ function generateCustomerInfo(doc, invoice) {
     .text("Balance Due:", 50, position + 30)
     .text(
       formatCurrency(invoice.subtotal - invoice.amount_paid),
-      150, 
+      150,
       position + 30
-  )
+    )
     .text("Terms:", 50, position + 45)
     .text(invoice.terms, 150, position + 45)
 
-  .font("Helvetica-Bold")
-  .text(ship.name, 300, position)
-  .font("Helvetica")
-  .text(ship.address, 300, position + 15)
-  .text(
-    `${ship.city}, ${ship.state}, ${ship.country}`,
-    300,
-    position + 30
-  )
-  .text(ship.email, 300, position + 45)
-  .moveDown();
+    .font("Helvetica-Bold")
+    .text(ship.name, 300, position)
+    .font("Helvetica")
+    .text(ship.address, 300, position + 15)
+    .text(`${ship.city}, ${ship.state}, ${ship.country}`, 300, position + 30)
+    .text(ship.email, 300, position + 45)
+    .moveDown();
 
   generateHomeRow(doc, 267);
-
 }
 
 function generateInvoiceTable(doc, invoice) {
@@ -93,9 +192,9 @@ function generateInvoiceTable(doc, invoice) {
       doc,
       position,
       item.item_description,
-      formatCurrency(item.amount / item.quantity),
+      formatCurrency(item.amount),
       item.quantity,
-      formatCurrency(item.amount)
+      formatCurrency(item.amount * item.quantity)
     );
 
     generateHomeRow(doc, position + 20);
@@ -134,15 +233,13 @@ function generateInvoiceTable(doc, invoice) {
   doc.font("Helvetica");
 }
 
-
-
 function generateHomeRow(doc, y_pos) {
   doc
-  .strokeColor("#aaaaaa")
-  .lineWidth(1)
-  .moveTo(50, y_pos)
-  .lineTo(550, y_pos)
-  .stroke();
+    .strokeColor("#aaaaaa")
+    .lineWidth(1)
+    .moveTo(50, y_pos)
+    .lineTo(550, y_pos)
+    .stroke();
 }
 
 function formatCurrency(cents) {
@@ -176,14 +273,7 @@ function generateTableRow(
 function generateFooter(doc, invoice) {
   doc
     .fontSize(10)
-    .text(
-      invoice.notes,
-      50,
-      780,
-      { align: "center", width: 500 }
-    );
+    .text(invoice.notes, 50, 700, { align: "center", width: 500 });
 }
 
-module.exports = { generatePDF };
-
-
+module.exports = { generateInvoiceFromRequestObject, generatePDF };
