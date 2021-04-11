d1974f0b4bfba247f0c3984ac1640af9e046cbb8
Richard Coucoules <richardcoucoules@gmail.com>
2021-04-11 08:44:48 -0700

    Pipe html form data to generate-pdf

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const PORT = 3000;
 const express = require("express");
+const Busboy = require("busboy");
 const pdf = require("./modules/generate-pdf");
 
 const app = express();
@@ -10,8 +11,18 @@ app.get("/", (req, res) => {
 });
 
 app.post("/generate-pdf", (req, res) => {
-  res.send({ endpoint: "/generate-pdf POST", ...req.body });
-  // pdf(req.body);
+  const busboy = new Busboy({ headers: req.headers });
+  const body = {};
+  busboy.on("field", function (fieldname, val) {
+    body[fieldname] = val;
+  });
+  busboy.on("finish", function () {
+    console.log(body);
+    const invoice = pdf.generateInvoiceFromRequestObject(body);
+    pdf.generatePDF(invoice, "invoice.pdf");
+    res.send({ endpoint: "/generate-pdf POST", body });
+  });
+  req.pipe(busboy);
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
